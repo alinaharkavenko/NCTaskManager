a92926203cd58045a8016aa68fedc3c19ed92e44
alinaharkavenko <alina5557alina@gmail.com>
2021-12-19 11:34:19 +0200

    Practice 10

--- src/main/java/ua/edu/sumdu/j2se/harkavenko/tasks/ListTypes.java
+++
@@ -1,7 +0,0 @@
-package ua.edu.sumdu.j2se.harkavenko.tasks;
-
-public class ListTypes {
-    public enum types{
-        ARRAY, LINKED
-    }
-}
--- src/main/java/ua/edu/sumdu/j2se/harkavenko/tasks/Main.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.sumdu.j2se.harkavenko.tasks;
-
-public class Main {
-
-	public static void main(String[] args) {
-
-
-	}
-}
